09b4debc84a1f8d65425635a03504a2b51fac764
Manish Goud <76445347+MANISHGOUD96@users.noreply.github.com>
2023-01-30 15:15:14 +0530

    Add files via upload

---
+++ EmpController.cs
@@ -0,0 +1,67 @@
+﻿using MK_web_API_Project.DB_Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MK_web_API_Project.Controllers
+{
+    public class EmpController : ApiController
+    {
+        
+        REVEntities DB = new REVEntities();
+
+        [HttpGet]
+        [Route("Api/GetEMPData")]
+        public List<Employee> GetData()
+        {
+            var rev = DB.Employees.ToList();
+            return rev;
+        }
+         
+        [HttpPost]
+        [Route("Api/SaveData")]
+        
+        public HttpResponseMessage SaveEmp(Employee obj)
+        {
+            if (obj.Id == 0)
+            {
+                DB.Employees.Add(obj);
+                DB.SaveChanges();
+            }
+            else
+            {
+                DB.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+                DB.SaveChanges();
+            }
+            HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.OK);
+            return res;
+        }
+        
+
+        [HttpGet]
+        [Route("Api/EditData")]
+
+        public Employee GetEditData(int Id)
+        {
+            var resedit = DB.Employees.Where(a => a.Id == Id).First();
+            return resedit;
+        }
+
+        [HttpGet]
+        [Route("Api/DeleteData")]
+
+        public HttpResponseMessage DeleteData(int Id)
+        {
+            var delete = DB.Employees.Where(a => a.Id == Id).First();
+            DB.Employees.Remove(delete);
+            DB.SaveChanges();
+            HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.OK);
+            return res;
+
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MK_web_API_Project.WebApiApplication" Language="C#" %>
